a9bb16e2210f39b7a1f3ce1e46f25698ebe31cbc
Uros Milosavljevic <urosmilosavljevic63@gmail.com>
2023-03-09 20:22:11 +0100

    Setup path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@core/ui': path.resolve(__dirname, './src/core/ui/index.ts'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+    },
+  },
 });
