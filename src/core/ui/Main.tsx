a2764ef5c1bbb1da7c7090782258a7184b1dbe6c
Uros Milosavljevic <urosmilosavljevic63@gmail.com>
2023-03-11 18:36:47 +0100

    Add no nested routes check

--- src/core/ui/Main.tsx
+++ src/core/ui/Main.tsx
@@ -1,4 +1,3 @@
-// eslint-disable-next-line react/prop-types
 export const Main: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div className="grid p-6" style={{ height: `calc(100vh - 4rem)` }}>
